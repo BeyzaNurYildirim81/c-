4bb86b2c21453c5aed3709c18557ed3f58394647
BeyzaNurYildirim81 <megazeka09@gmail.com>
2021-03-30 22:19:28 +0300

    İlk proje oluşturuldu . Türkçe karakter kullanımı sağlandı.

---
+++ hello world.cpp
@@ -0,0 +1,15 @@
+﻿// hello world.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+   setlocale(LC_ALL, "Turkish");
+
+   cout << "Hello world \n";
+   cout << "Merhaba Dünya";
+}
+
